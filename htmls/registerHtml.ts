23ae1a01f3b971b5c65253207b548fe8cbd795db
Timothy Chia <timothycth9@gmail.com>
2023-08-16 13:47:22 +0800

    asd

--- htmls/registerHtml.ts
+++ htmls/registerHtml.ts
@@ -1,7 +1,6 @@
 import { env } from "@/env.mjs";
 
 export function registerHtml(token: string) {
-  console.log('i love cum, ', env.URL)
   return `
   <!DOCTYPE html>
   <html>
@@ -183,7 +182,7 @@ export function registerHtml(token: string) {
                       <table border="0" cellpadding="0" cellspacing="0">
                         <tr>
                           <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                            <a href="${env.URL}/verify-email?token=${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a>
+                            <a href="${env.URL}verify-email?token=${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a>
                           </td>
                         </tr>
                       </table>
