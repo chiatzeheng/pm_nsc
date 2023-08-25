b7a7f6291a9ccde15209f4a2c14dcc491e53e489
Timothy Chia <timothycth9@gmail.com>
2023-08-25 09:09:37 +0800

    asf

--- htmls/passwordHtml.ts
+++ htmls/passwordHtml.ts
@@ -182,7 +182,7 @@ export function passwordHtml(token: string) {
                       <table border="0" cellpadding="0" cellspacing="0">
                         <tr>
                           <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                            <a href="${env.URL}reset-password/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Reset Password</a>
+                            <a href= "https://pm-nsc.vercel.app/reset-password/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Reset Password</a>
                           </td>
                         </tr>
                       </table>
